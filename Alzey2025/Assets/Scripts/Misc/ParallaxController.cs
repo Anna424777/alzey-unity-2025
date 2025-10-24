5e02406476ad45f9a23b4a97c83326711cc30b4a
Anna424777 <anna.charlotte.pinger@gmail.com>
2025-10-24 15:17:51 +0200

    Tag 5 - Hintergrund & fertigstellung

--- Alzey2025/Assets/Scripts/Misc/ParallaxController.cs
+++ Alzey2025/Assets/Scripts/Misc/ParallaxController.cs
@@ -1,37 +1,43 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Misc {
     public class ParallaxController : MonoBehaviour {
         /// <summary>
         /// How fast the background moves relative to the camera. 0 = moves with cam || 1 won't move
         /// </summary>
+        [SerializeField, Range(0, 1), Tooltip("0 moves with cam || 1 won't move"), FormerlySerializedAs("modifier")]
+        private float modifierX;
         [SerializeField, Range(0, 1), Tooltip("0 moves with cam || 1 won't move")]
-        private float modifier;
+        private float modifierY;
 
         private Camera cam;
-        private float startPosition;
+        private float startPositionX;
+        private float startPositionY;
         private float width;
 
         private void Start() {
             width = GetComponent<SpriteRenderer>().bounds.size.x;
             cam = Camera.main;
-            startPosition = transform.position.x;
+            startPositionX = transform.position.x;
+            startPositionY= transform.position.y;
         }
 
         private void FixedUpdate() {
             // Calculate distance background move base on cam movement
-            float distance = cam.transform.position.x * modifier;
-            float movement = cam.transform.position.x * (1 - modifier);
+            float distanceX = cam.transform.position.x * modifierX;
+            float distanceY = cam.transform.position.y * modifierY;
+            float movement = cam.transform.position.x * (1 - modifierX);
 
-            transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);
+            transform.position = new Vector3(startPositionX + distanceX, startPositionY + distanceY, transform.position.z);
 
             // If background has reached the end of its length, adjust position for infinite scrolling
-            if (movement > startPosition + width) {
-                startPosition += width;
+            if (movement > startPositionX + width) {
+                startPositionX += width;
             }
 
-            if (movement < startPosition - width) {
-                startPosition -= width;
+            if (movement < startPositionX - width) {
+                startPositionX -= width;
             }
         }
     }
